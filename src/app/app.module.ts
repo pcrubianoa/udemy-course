1874c025b9dacf238af744f2ae419c4e5e63349a
pablo <pcrubianoa@gmail.com>
2017-10-01 18:14:11 -0500

    Routes ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routes
+import { app_routing } from './app.routes';
+
+
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -18,7 +23,8 @@ import { ProductComponent } from './components/product/product.component';
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import {
+    AboutComponent,
+    PortafolioComponent,
+    ProductComponent
+ } from './components/index.paginas';
+
+const app_routes: Routes = [
+    { path: '', component: PortafolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'product', component: ProductComponent },
+    { path: '**', pathMatch: 'full', redirectTo: '' }
+];
+export const app_routing = RouterModule.forRoot(app_routes, { useHash: true });
