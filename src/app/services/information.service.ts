b8530a03171101fa85978eefa9bd3f51257d1bbe
pablo <pcrubianoa@gmail.com>
2017-10-02 17:42:41 -0500

    Firebase ok

--- src/app/services/information.service.ts
+++ src/app/services/information.service.ts
@@ -6,14 +6,30 @@ export class InformationService {
 
   info: any = {};
   loaded: boolean = false;
+  loaded_about: boolean = false;
+  team: any[] = [];
 
   constructor( public http:Http ) { 
+    this.set_info();
+    this.set_about();
+  }
+
+  public set_info()
+  {
     this.http.get('assets/data/info.pagina.json')
     .subscribe(data => {
-      console.log(data.json());
+      // console.log(data.json());
       this.info = data.json();
       this.loaded = true;
     });
   }
-
+  public set_about()
+  {
+    this.http.get('https://portafolio-c0285.firebaseio.com/team.json')
+    .subscribe(data => {
+      console.log(data.json());
+      this.team = data.json();
+      this.loaded = true;
+    });
+  }
 }
